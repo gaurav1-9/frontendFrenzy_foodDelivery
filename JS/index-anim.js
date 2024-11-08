b57ca769c39bbc7ccd28a6a5a0e807887af1f03a
Gaurav <gauravkrdas19@gmail.com>
2024-11-08 16:37:08 +0530

    gsap animation index page

---
+++ JS/index-anim.js
@@ -0,0 +1,28 @@
+var tl = gsap.timeline();
+
+tl.from('.nav-logo',{
+    y:-100,
+    delay:0.2,
+    duration:0.3,
+    stagger: 0.2,
+    opacity:0,
+})
+tl.from('.nav-part2 .links',{
+    y:-100,
+    duration:0.3,
+    stagger: 0.2,
+    opacity:0,
+})
+tl.from('.left h1, p',{
+    opacity: 0,
+    x:-100,
+    stagger:0.1,
+})
+tl.from('.btns',{
+    opacity:0,
+    y:20,
+})
+tl.from('.right',{
+    opacity:0,
+    scale:0,
+},'-=1')
